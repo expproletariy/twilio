bb4e3172446abdac8cf8aa933e2d55cd378d690c
expproletariy <expproletariy@gmail.com>
2020-03-06 12:05:11 +0300

    v0.0.10 -  fix query result type

--- autopilot/queries/types/query_result.go
+++ autopilot/queries/types/query_result.go
@@ -2,7 +2,7 @@ package types
 
 type QueryResult struct {
 	Task           string  `json:"task"`
-	TaskConfidence float32 `json:"task_confidence"`
+	TaskConfidence string  `json:"task_confidence"`
 	NextBestTask   string  `json:"next_best_task"`
 	Fields         []Field `json:"fields"`
 }
